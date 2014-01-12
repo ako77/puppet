a3fe95110503d85bfd4485417b82d3d2a3f37fbf
AKO <ako77@arcor.de>
2014-01-12 18:46:09 +0100

    Accounts changes, sshkeys

--- modules/accounts/manifests/virtual.pp
+++ modules/accounts/manifests/virtual.pp
@@ -1,6 +1,10 @@
 # Defined type for creating virtual users accounts
 #
-define accounts::virtual($uid,$realname,$pass) {
+define accounts::virtual($uid,$realname,$pass,$sshkeytype,$sshkey) {
+#  include accounts::params
+  # Pull in values from accounts::params
+#  $homepath = $accounts::params::homepath
+#  $shell = $accounts::params::shell
 
   user { $title :
     ensure     => present,
@@ -24,5 +28,26 @@ define accounts::virtual($uid,$realname,$pass) {
     group      => $title,
     mode       => '0750',
     require    => [ User[$title], Group[$title] ],
+  }
+
+  # Ensure the .ssh directory exists with the right permissions
+  file { "/home/${title}/.ssh" :
+    ensure     => directory,
+    owner      => $title,
+    group      => $title,
+    mode       => '0700',
+    require    => File["/home/${title}"],
+  }
+
+  # Add user's SSH key
+  if ($sshkey != '' and $sshkeytype != '') {
+    ssh_authorized_key { $title :
+      ensure   => present,
+      name     => $title,
+      user     => $title,
+      type     => $sshkeytype,
+      key      => $sshkey,
+      require => File["/home/${title}/.ssh"]
+    }
   }
 }
