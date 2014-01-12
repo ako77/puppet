54c385ac6d3371f22b074b46be05ac59532543da
AKO <ako77@arcor.de>
2014-01-13 00:59:40 +0100

    ntp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -6,4 +6,7 @@ node 'cmmaster', 'lnx-01' {
   exec { 'Run arbitrary command' :
     command => '/bin/echo Ok, last ran papply on `/bin/date` > /tmp/command.output.txt',
   }
+  class { 'ntp':
+    servers => [ '192.168.10.254', 'ntp1.ptb.de', 'ntp2.ptb.de' ],
+  }
 }
