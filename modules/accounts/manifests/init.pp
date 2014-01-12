d26dead7add4ed5c5a10f9df96f9a72135f62d0a
AKO <ako77@arcor.de>
2014-01-12 18:05:22 +0100

    changes

--- modules/accounts/manifests/init.pp
+++ modules/accounts/manifests/init.pp
@@ -5,16 +5,19 @@ class accounts {
   @accounts::virtual { 'ead' :
     uid      => 1001,
     realname => 'System Operator',
-    pass     => '',
+    pass     => '$6$uz2FWMAEGCpD91SJ$aFub4P9h8O.nBpeGc3vtFUcGMqU9PpPiyN9KGcPzCktBxx2bZ1Wxi.zxsrKLkPX5205JO0x9op1si2SruvHnB0',
+    #require  => Class['accounts::config'],
   }
   @accounts::virtual { 'sysop' :
     uid      => 1002,
     realname => 'System Operator',
-    pass     => '',
+    pass     => '$6$uz2FWMAEGCpD91SJ$aFub4P9h8O.nBpeGc3vtFUcGMqU9PpPiyN9KGcPzCktBxx2bZ1Wxi.zxsrKLkPX5205JO0x9op1si2SruvHnB0',
+    #require  => Class['accounts::config'],
   }
   @accounts::virtual { 'mku' :
     uid      => 1003,
     realname => 'Markus Künstler',
-    pass     => '',
+    pass     => '$6$dIJeZPsY/mI$VGgVVsFERsUHcFlJ9xJ76malX4j4Dztz6a4H5YWd8CBqghbvD1SFiZj4gHHMpO.dzvdJWKG3EHRLBJ/8pCQ7G.',
+    #require  => Class['accounts::config'],
   }
 }
