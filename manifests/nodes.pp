e2d18b98b3bf1a998cc1134cf197c3ad78351352
AKO <ako77@arcor.de>
2014-01-18 15:28:02 +0100

    Change nodes, adding lnx-02,03

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -10,3 +10,29 @@ node 'cmmaster', 'lnx-01' {
     servers => [ '192.168.10.254', 'ntp1.ptb.de', 'ntp2.ptb.de' ],
   }
 }
+
+node 'lnx-02' {
+  include puppet
+  include sudoers
+  include accounts
+  realize (Accounts::Virtual['ead', 'sysop', 'mku'])
+  exec { 'Run arbitrary command' :
+    command => '/bin/echo Ok, last ran papply on `/bin/date` > /tmp/command.output.txt',
+  }
+  class { 'ntp':
+    servers => [ '192.168.10.254', 'ntp1.ptb.de', 'ntp2.ptb.de' ],
+  }
+}
+
+node 'lnx-03' {
+  include puppet
+  include sudoers
+  include accounts
+  realize (Accounts::Virtual['ead', 'sysop', 'mku'])
+  exec { 'Run arbitrary command' :
+    command => '/bin/echo Ok, last ran papply on `/bin/date` > /tmp/command.output.txt',
+  }
+  class { 'ntp':
+    servers => [ '192.168.10.254', 'ntp1.ptb.de', 'ntp2.ptb.de' ],
+  }
+}
--- modules/accounts/manifests/init.pp
+++ modules/accounts/manifests/init.pp
@@ -8,7 +8,7 @@ class accounts {
     pass       => '$6$uz2FWMAEGCpD91SJ$aFub4P9h8O.nBpeGc3vtFUcGMqU9PpPiyN9KGcPzCktBxx2bZ1Wxi.zxsrKLkPX5205JO0x9op1si2SruvHnB0',
     sshkeytype => 'ssh-rsa',
     sshkey     => 'AAAAB3NzaC1yc2EAAAABJQAAAQBhWrCVlRU+7M43dSC+InWXNTQiRX9yI3wtt3ek4Jr8URPM6Daep6JJhVhsHFua8LMk/Sga/jysQXJlBmPxa6XSMeAhKXwtQy3zrx8e6QvM4YKpeJho+21LhHdZmtDWgcdS9GIsO1fLC+3a8osTOznHMqghW0Iy9B6b+CeG+/X/B37UIjoaH5swY8sh53wzpx0lBbDX5b/vDadYlHKagtTU1ghMk+OvHcuWEjg3Hzu3D+uuY9GVY6cEJbPuGFh0hvWz2+KnTQqN4JpDjd5Wh/VIw6PPWhkF9ulSVTVVFsTCokPyMCClYEVy73geqHi8YxYNu4KFcgnrQsQZS1eXOnxP',
-    #require  => Class['accounts::config'],
+    #require    => Class['accounts::config'],
   }
   @accounts::virtual { 'sysop' :
     uid        => 1002,
@@ -16,7 +16,7 @@ class accounts {
     pass       => '$6$uz2FWMAEGCpD91SJ$aFub4P9h8O.nBpeGc3vtFUcGMqU9PpPiyN9KGcPzCktBxx2bZ1Wxi.zxsrKLkPX5205JO0x9op1si2SruvHnB0',
     sshkeytype => '',
     sshkey     => '',
-    #require  => Class['accounts::config'],
+    #require    => Class['accounts::config'],
   }
   @accounts::virtual { 'mku' :
     uid        => 1003,
@@ -24,6 +24,6 @@ class accounts {
     pass       => '$6$dIJeZPsY/mI$VGgVVsFERsUHcFlJ9xJ76malX4j4Dztz6a4H5YWd8CBqghbvD1SFiZj4gHHMpO.dzvdJWKG3EHRLBJ/8pCQ7G.',
     sshkeytype => '',
     sshkey     => '',
-    #require  => Class['accounts::config'],
+    #require    => Class['accounts::config'],
   }
 }
