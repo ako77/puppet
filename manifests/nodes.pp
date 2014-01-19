27154f1437c8431e5b30e83e0ca4a283afa4a40a
AKO <ako77@arcor.de>
2014-01-19 15:58:15 +0100

    del module nginxphp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -27,42 +27,6 @@ node 'lnx-02' {
   class { 'ntp':
     servers => [ '192.168.10.254', 'ntp1.ptb.de', 'ntp2.ptb.de' ],
   }
-  ### Install php 5.4 ppa.
-  class { 'nginxphp::ppa': stage => 'setup' }
-  ### Initiate the module base requirements.
-  include nginxphp
-  ### Install php-fpm with the modules you desire.
-  class { 'nginxphp::php':
-    php_packages => [
-      "php5-intl",
-      "php5-curl",
-      "php5-gd",
-      "php5-xcache",
-      "php5-mcrypt",
-      "php5-xmlrpc",
-      "php5-xsl",
-      "php5-imagick",
-      "php5-mysql",
-    ],
-    withppa      => true
-  }
-  ### Install Nginx
-  include nginxphp::nginx
-  ### Install PHP developement tools.
-  include nginxphp::phpdev
-  ### Configure FPM Pool
-  nginxphp::fpmconfig { 'ubuntu':
-    php_devmode   => true,
-    fpm_user      => 'www-data',
-    fpm_group     => 'www-data',
-    fpm_allowed_clients => ''
-  }
-  ### Configure Nginx site
-  nginxphp::nginx_addphpconfig { 'lnx-02.kitenet.loc':
-    website_root       => "/var/www/",
-    default_controller => "index.php",
-    require => Nginxphp::Fpmconfig['ubuntu']
-  }
 }
 
 
@@ -97,7 +61,7 @@ node 'lnx-03' {
     grant       => ['ALL'],
   }
   class { '::mysql::bindings':
-    php_enable => 1,
-    perl_enable => 1
+    php_enable => 0,
+    perl_enable => 1,
   }
 }
