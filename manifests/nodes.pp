9f4889142fbaa38cb9efe1905b56aec8f5cd08d5
System Operator <ubuntu@lnx-03.kitenet.loc>
2014-01-19 05:37:36 +0100

    adding nginx php-fpm to lnx-02

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -26,6 +26,42 @@ node 'lnx-02' {
   class { 'ntp':
     servers => [ '192.168.10.254', 'ntp1.ptb.de', 'ntp2.ptb.de' ],
   }
+  ### Install php 5.4 ppa.
+  class { 'nginxphp::ppa': stage => 'setup' }
+  ### Initiate the module base requirements.
+  include nginxphp
+  ### Install php-fpm with the modules you desire.
+  class { 'nginxphp::php':
+    php_packages => [
+      "php5-intl",
+      "php5-curl",
+      "php5-gd",
+      "php5-xcache",
+      "php5-mcrypt",
+      "php5-xmlrpc",
+      "php5-xsl"
+      "php5-imagick"
+      "php5-mysql"
+    ],
+    withppa      => true
+  }
+  ### Install Nginx
+  include nginxphp::nginx
+  ### Install PHP developement tools.
+  include nginxphp::phpdev
+  ### Configure FPM Pool
+  nginxphp::fpmconfig { 'ubuntu':
+    php_devmode   => true,
+    fpm_user      => 'www-data',
+    fpm_group     => 'www-data',
+    fpm_allowed_clients => ''
+  }
+  ### Configure Nginx site
+  nginxphp::nginx_addphpconfig { 'cmmaster.kitenet.loc':
+    website_root       => "/var/www/",
+    default_controller => "index.php",
+    require => Nginxphp::Fpmconfig['ubuntu']
+  }
 }
 
 
