e05a3b65ab77e1fb3d9a50e15b24f9401d0df918
AKO <ako77@arcor.de>
2014-01-24 02:50:06 +0100

    cleaning node part lnx-03

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -27,16 +27,7 @@ node 'lnx-02' inherits cmmaster {
 #==============================================================================
 
 node 'lnx-03' inherits cmmaster {
-#  include puppet
-#  include sudoers
-#  include accounts
-#  realize (Accounts::Virtual['ead', 'sysop', 'mku'])
-#  exec { 'Run arbitrary command' :
-#    command => '/bin/echo Ok, last ran papply on `/bin/date` > /tmp/command.output.txt',
-#  }
-#  class { 'ntp':
-#    servers => [ '192.168.10.254', 'ntp1.ptb.de', 'ntp2.ptb.de' ],
-#  }
+
   class { '::mysql::server':
     package_ensure   => latest,
     root_password    => 'strongpassword',
